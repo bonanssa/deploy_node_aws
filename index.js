afabce9bc9a7c7410f20a4097344667da676802c
Alex Souza <souzzalex@gmail.com>
2022-07-12 11:41:15 -0300

    primeiro envio

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = request('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Server is up ' });
+});
+
+app.listen(3333);
